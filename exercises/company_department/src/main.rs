03ebefde27ab5fcb1e91a60635841b29472235f3
Petr Spac <petrik.spac@gmail.com>
2022-09-08 23:23:19 +0200

    Add user to department

--- exercises/company_department/src/main.rs
+++ exercises/company_department/src/main.rs
@@ -1,4 +1,55 @@
-fn main() {
-    println!("Hello, world!");
-}
+use std::collections::HashMap;
+use std::io::{self, BufRead};
 
+fn main() {
+    let mut db: HashMap<String, Vec<String>> = HashMap::new();
+
+    println!("Type 'Add <name> to <department>' to add an employee");
+    println!("Type 'List <department>' to list the employees of a department");
+    println!("Type 'All' to list all employees by department");
+    println!("Type 'Quit' to quit");
+
+    let stdin = io::stdin();
+    // TODO: read more about reading input lines
+    for line in stdin.lock().lines() {
+        let line = line.unwrap();
+        let mut words = line.split_whitespace();
+        let command = words.next().unwrap();
+        match command {
+            "Add" => {
+                let name = words.next().unwrap();
+                let to = words.next().unwrap();
+                let department = words.next().unwrap();
+                if to != "to" {
+                    println!("Expected 'to' but got '{}'", to);
+                    continue;
+                }
+
+                // Get or create here vector with employees
+                if db.contains_key(department) {
+                    db.get_mut(department).unwrap().push(name.to_string());
+                } else {
+                    // TODO: Maybe ask here if we want to create ne department
+                    db.insert(department.to_string(), vec![name.to_string()]);
+                }
+
+                println!("{:?}", db);
+                println!("Adding {} to {}", name, department);
+            }
+            "List" => {
+                let department = words.next().unwrap();
+                println!("Listing employees of {:?}", db);
+            }
+            "All" => {
+                println!("Listing all employees by department");
+            }
+            "Quit" => {
+                println!("Quitting");
+                break;
+            }
+            _ => {
+                println!("Unknown command '{}'", command);
+            }
+        }
+    }
+}
